680f5b3f0982dd17af3c83c4aa7aa20d923d5d5c
Robert JONES <rojones@e4r21p1.jnb.42.fr>
2017-07-07 14:47:46 +0200

    find what is going wrong

--- server_src/ft_p_server.h
+++ server_src/ft_p_server.h
@@ -19,16 +19,36 @@
 
 typedef struct sockaddr_in	t_tcp_sock_in;
 
+typedef void(*builtin)(t_cmd cmd);
+
+typedef struct	s_builtin_cmd
+{
+	char 	*cmd;
+	builtin	fnc;
+}				t_builtin_cmd;
+
 typedef struct		s_svr_env
 {
 	int 			cln_cmd;
 	int 			svr_id;
 	char 			*svrroot;
-	char 			*home;
 	t_tcp_sock_in	sock_in;
 }					t_svr_env;
 
 extern t_svr_env	g_svr_env;
+
+void	ft_send_responce(t_cmd_rsp res);
+t_cmd_rsp ft_get_responce();
+void ft_send_cmd(t_cmd cmd);
 void ft_print_exit(char *str);
+
+void ft_quit(t_cmd cmd);
+void ft_put(t_cmd cmd);
+
 t_cmd	ft_get_cmd();
+
+static t_builtin_cmd g_builtin_cmd[] = {
+		{"QUIT", ft_quit},
+		NULL
+};
 #endif
---
+++ server_src/ft_put.c
@@ -0,0 +1,4 @@
+//
+// Created by Robert JONES on 2017/07/07.
+//
+
